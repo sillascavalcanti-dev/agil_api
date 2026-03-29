59e1d6b2f37ee61a81a480afd4662c2f61dda6b1
Sillas Cavalcanti <sillascavalcanti0@gmail.com>
2026-03-29 14:29:42 -0300

    chore: remove generated files from dist/ directory

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mensagem = "Projeto TypeScript iniciado!";
-console.log(mensagem);
-//# sourceMappingURL=index.js.map
